6c2b5be878cd67ffae516f200fd54fbc13e13ead
Daniel <dsalvatori2008@gmail.com>
2023-02-03 13:18:47 +0000

    edited server.js

--- Server/server.js
+++ Server/server.js
@@ -1,44 +1,22 @@
+const express = require('express');
+const app = express();
 
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const express = require('express');
-// const app = express();
-
-// const cors = require('cors');
-// app.use(cors());
-// app.use(express.json());
-
-// const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 // const createRouter = require('./helpers/create_router.js');
 
-// MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
-//   .then((client) => {
-//     const db = client.db('birds');
-//     const sightingsCollection = db.collection('sightings');
-//     const sightingsRouter = createRouter(sightingsCollection);
-//     app.use('/api/sightings', sightingsRouter);
-//   })
-//   .catch(console.err);
+MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
+  .then((client) => {
+    const db = client.db('BreakTime');
+    const diaryEntriesCollection = db.collection('diary_entries');
+    const diaryEntriesRouter = createRouter(diaryEntriesCollection);
+    app.use('/api/diaryEntries', diaryEntriesRouter);
+  })
+  .catch(console.err);
 
-// app.listen(9000, function () {
-//   console.log(`Listening on port ${ this.address().port }`);
-// });
+app.listen(9000, function () {
+  console.log(`Listening on port ${ this.address().port }`);
+});
