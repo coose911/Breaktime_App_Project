28f272616ba1447493596b77e34f801ba36de4dd
Marcin <marcinegzmont@gmail.com>
2023-02-06 13:15:56 +0000

    added increase functions for coffee and water

--- client/src/Components/BreaksComponent.js
+++ client/src/Components/BreaksComponent.js
@@ -1,43 +1,52 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const Bubble = styled.section`
-    border: #00AEAE solid 5px;
-    border-radius: 20px;
-`
+  border: #00aeae solid 5px;
+  border-radius: 20px;
+`;
 const RadioButtonsContainer = styled.form`
-    border: gray solid 2px;
-    border-radius: 20px;
-`
+  border: gray solid 2px;
+  border-radius: 20px;
+`;
 
 // Props = breaksTaken state & breakLength state
-const BreaksComponent = ({breaksTaken, breakLength}) => {
+const BreaksComponent = ({ breaksTaken, breakLength }) => {
+  return (
+    <Bubble>
+      <h2>Breaks Taken</h2>
+      <button /* onClick={onClick} */>+ BREAK</button>
 
-    return(
-        <Bubble>
-        <h2>Breaks Taken</h2>
-            <button /* onClick={onClick} */>+ BREAK</button>
+      <br />
+      <br />
+      <RadioButtonsContainer /* onSubmit={saveBreakLength} */>
+        <legend>Select your preferred break length:</legend>
+        <label>10 minutes</label>
+        <input
+          type="radio"
+          id="10"
+          name="break-length"
+          value="10" /* onClick={handleRadioButton} */
+        />
+        <br />
+        <label>20 minutes</label>
+        <input
+          type="radio"
+          id="20"
+          name="break-length"
+          value="20" /* onClick={handleRadioButton} */
+        />
+        <br />
+        <br />
+        <input type="submit" value="Set Length"></input>
+      </RadioButtonsContainer>
 
-            <br/>
-            <br/>
-            <RadioButtonsContainer /* onSubmit={saveBreakLength} */>
-                <legend>Select your preferred break length:</legend>
-                <label>10 minutes</label>
-                <input type="radio" id="10" name="break-length" value="10" /* onClick={handleRadioButton} */ />
-                <br/>
-                <label>20 minutes</label>
-                <input type="radio" id="20" name="break-length" value="20" /* onClick={handleRadioButton} */ />
-                <br/>
-                <br/>
-                <input type="submit" value="Set Length"></input>
-            </RadioButtonsContainer>
+      <br />
+      <br />
+      <ul>
+        <li>Breaks Taken: (value here) - Will be a bar showing breaks taken</li>
+      </ul>
+    </Bubble>
+  );
+};
 
-            <br/>
-            <br/>
-            <ul>
-            <li>Breaks Taken: (value here) - Will be a bar showing breaks taken</li>
-            </ul>
-        </Bubble>
-    )
-}
-
-export default BreaksComponent
+export default BreaksComponent;
--- client/src/Components/ConsumptionComponent.js
+++ client/src/Components/ConsumptionComponent.js
@@ -1,24 +1,43 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import { useState } from "react";
 
 const Bubble = styled.section`
-    border: #00AEAE solid 5px;
-    border-radius: 20px;
-`
+  border: #00aeae solid 5px;
+  border-radius: 20px;
+`;
 
 // Props = waterDrank state & coffeeDrank state
-const ConsumptionComponent = ({waterDrank, coffeeDrank}) => {
+const ConsumptionComponent = ({ waterDrank, coffeeDrank }) => {
+  const [percentWater, setPercentWater] = useState(0);
+  const [percentCoffee, setPercentCoffee] = useState(0);
 
-    return (
-        <Bubble>
-            <h2>Water & Coffee Drunk</h2>
-                <button /* onClick={onClickWater} */>+ COFFEE</button>
-                <button /* onClick={onClickCoffee} */>+ WATER</button>
-                <ul>
-                    <li>Water drank: (value here) - Will be a bar showing water drank</li>
-                    <li>Coffee drank: (value here) - Will be a bar showing coffee drank</li>
-                </ul>
-        </Bubble>
-    )
-}
+  const increaseWater = () => {
+    if (percentWater + 10 > 100) return;
+    setPercentWater(percentWater + 10);
+  };
+  const increaseCoffee = () => {
+    if (percentCoffee + 10 > 100) return;
+    setPercentCoffee(percentCoffee + 10);
+  };
 
-export default ConsumptionComponent
+  //   const decrease = () => {
+  //     if (percent - 10 < 0) return;
+  //     setPercent(percent - 10);
+  //   };
+
+  return (
+    <Bubble>
+      <h2>Water & Coffee Drunk</h2>
+      <button onClick={increaseCoffee}>+ COFFEE</button>
+      <button onClick={increaseWater}>+ WATER</button>
+      <ul>
+        <li>Water drank: {percentWater} - Will be a bar showing water drank</li>
+        <li>
+          Coffee drank: {percentCoffee} - Will be a bar showing coffee drank
+        </li>
+      </ul>
+    </Bubble>
+  );
+};
+
+export default ConsumptionComponent;
