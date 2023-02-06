1dbf4e734ce3a2d571c58ad552cd74c9601b51d6
Marcin <marcinegzmont@gmail.com>
2023-02-06 15:45:23 +0000

    commit?

--- client/src/helpers/DiaryService.js
+++ client/src/helpers/DiaryService.js
@@ -1,21 +1,18 @@
-const diaryURL = 'http://localhost:9000/api/diary_entries/'
+const diaryURL = "http://localhost:9000/api/diary_entries/";
 
 export const getDiaryEntries = () => {
-    return fetch(diaryURL)
-    .then(res => res.json())
-}
+  return fetch(diaryURL).then((res) => res.json());
+};
 
 export const getSingleDiaryEntry = (id) => {
-    return fetch(diaryURL + id)
-    .then(res => res.json())
-}
+  return fetch(diaryURL + id).then((res) => res.json());
+};
 
 export const postDiaryEntry = (payload) => {
-    console.log(payload)
-    return fetch(diaryURL, {
-        method: 'POST',
-        body: JSON.stringify(payload),
-        headers: { 'Content-Type': 'application/json' }
-    })  
-    .then(res => res.json())
-}
+  console.log(payload);
+  return fetch(diaryURL, {
+    method: "POST",
+    body: JSON.stringify(payload),
+    headers: { "Content-Type": "application/json" },
+  }).then((res) => res.json());
+};
