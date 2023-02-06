62547d3145c34c446bfb13929ac2714431a8cab7
Char Broadley <broadleychar@gmail.com>
2023-02-06 14:27:47 +0000

    added consumables database to server.js - client and server linked

--- Server/server.js
+++ Server/server.js
@@ -11,14 +11,20 @@ const createRouter = require('./helpers/create_router.js');
 MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
   .then((client) => {
     const db = client.db('BreakTime');
+
+    
     const diaryEntriesCollection = db.collection('diary_entries');
     const diaryEntriesRouter = createRouter(diaryEntriesCollection);
     app.use('/api/diary_entries', diaryEntriesRouter);
-    const recordsCollection = db.collection('records');
-    const recordsRouter = createRouter(recordsCollection);
-    app.use('/api/records', recordsRouter);
+
+    const consumablesDataCollection = db.collection('consumables_data');
+    const consumablesDataRouter = createRouter(consumablesDataCollection);
+    app.use('/api/consumables_data', consumablesDataRouter);
+  
   })
   .catch(console.err);
+
+
 
 app.listen(9000, function () {
   console.log(`Listening on port ${ this.address().port }`);
